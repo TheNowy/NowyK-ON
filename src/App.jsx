03b673b147ec87cba1d2052b68b943790e143b3c
NowyTeam <jaj2232ajn@gmail.com>
2023-07-12 23:07:58 +0600

    FixBugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Wrapper } from "./components/wrapper/Wrapper";
+import PropagateLoader from "react-spinners/PropagateLoader";
+import "./App.scss"
 
 export const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, []);
+
   return (
     <>
-      <Wrapper />
+      {loading ? (
+        <div className="loader">
+          <PropagateLoader color="white" loading={loading} size={30} /> 
+        </div>
+      ) : (
+        <Wrapper />
+      )}
     </>
   );
 };
